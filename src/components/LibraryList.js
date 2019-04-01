6a28226448068a1258f76c4ade965ac3aef515a3
Louis Shi <Shilou2010@gmail.com>
2019-03-31 23:29:10 -0400

    librarylist mapStateToProps

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { View, Text } from 'react-native';
 
 class LibraryList extends Component {
   render() {
-    return;
+    console.log(this.props)
+    return <Text>hello</Text>
   }
 }
 
-export default LibraryList;
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
