64dc1697dff78c2f52fcf8ff3fd213ea5e0fba9f
Louis Shi <Shilou2010@gmail.com>
2019-04-01 19:01:31 -0400

    actions and reducers

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+  renderItem(library){
+    return <ListItem library={library} />
+  }
   render() {
-    console.log(this.props)
-    return <Text>hello</Text>
+    return ( 
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={(library) => library.id}
+      />
+    )
   }
 }
 
